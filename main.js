0555e08bae9f8f9b6a97e5dd605aa3fcb43218b5
khalid el masnaoui <khalidelmasnaoui1@gmail.com>
2019-11-14 20:00:57 +0100

    test0

--- main.js
+++ main.js
@@ -1,21 +1,41 @@
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    document.getElementById("signIn").style.display = "none";
-    document.getElementById("signOut").style.display = "block";
+// function onSignIn(googleUser) {
+//     var profile = googleUser.getBasicProfile();
+//     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+//     console.log('Name: ' + profile.getName());
+//     console.log('Image URL: ' + profile.getImageUrl());
+//     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+//     document.getElementById("signIn").style.display = "none";
+//     document.getElementById("signOut").style.display = "block";
 
+// }
+
+// function signOut() {
+//     var auth2 = gapi.auth2.getAuthInstance();
+//     auth2.signOut().then(function() {
+//         console.log('User signed out.');
+//     });
+//     document.getElementById("signIn").style.display = "block";
+//     document.getElementById("signOut").style.display = "none";
+
+
+// }
+
+function onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
 }
 
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function() {
-        console.log('User signed out.');
+function onFailure(error) {
+    console.log(error);
+}
+
+function renderButton() {
+    gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
     });
-    document.getElementById("signIn").style.display = "block";
-    document.getElementById("signOut").style.display = "none";
-
-
 }
